31e151351870450d70fea97149348f368bee6798
MattCordell <5741701+MattCordell@users.noreply.github.com>
2025-08-19 15:43:34 +1000

    Fix PatientReception deployment

--- PatientReception/vite.config.js
+++ PatientReception/vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
-  plugins: [react()],
-  // Ensure correct paths when deploying to GitHub Pages under /PatientReception/
-  base: '/PatientReception/',
+plugins: [react()],
+base: '/PatientReception/',
 });
